02c5ecbe684ea70f76f04016f09372fd239b5610
Anand Gupta <anand.gupta@olx.com>
2022-11-14 15:40:46 +0530

    Added location code

---
+++ track.js
@@ -0,0 +1,54 @@
+const trackBtn = document.getElementById('track-btn');
+const log = document.getElementById('log');
+
+let isTracking = false;
+let trackingIntervalId;
+let currentData;
+
+trackBtn.onclick = () => {
+
+  if (isTracking) {
+    stopTrack();
+  } else {
+    startTrack();
+  }
+}
+
+function stopTrack() {
+  isTracking = false;
+  window.clearInterval(trackingIntervalId)
+}
+
+function startTrack() {
+  
+  currentData = {};
+  trackingIntervalId = window.setInterval(() => {
+    collectEntry();
+    console.log('##############', currentData)
+    log.innerText = JSON.stringify(currentData);
+  }, 3000);
+  
+  isTracking = true;
+}
+
+function collectEntry() {
+  if (navigator.geolocation) {
+    navigator.geolocation.getCurrentPosition(position => {
+      currentData = toObj(position)
+    });
+  } else { 
+    console.error("Geolocation is not supported by this browser.")
+  }
+}
+
+
+function toObj(obj) {
+  if (obj === null || !(obj instanceof Object)) {
+    return obj;
+  }
+  const tmp = (obj instanceof Array) ? [] : {};
+  for (const key in obj) {
+    tmp[key] = toObj(obj[key]);
+  }
+  return tmp;
+}
